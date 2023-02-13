e137fb3ea14334efe2925b1e35adaf8d0940b6aa
mynameishtoomyataung <htoomyatag.webdev@gmail.com>
2023-02-13 17:45:45 +0700

    faker gem added for seed data purpose

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+
+Role.create([{ name: "Administrator" }, { name: "Seller" }])
+User.create(name: "luigi",password: "password",role_id:1)
+
+10.times do
+    User.create(
+    	author_pseudonym: Faker::Book.author, 
+    	name: Faker::Name.unique.name, 
+    	password: 'password', 
+    	role_id: 2)
+
+    50.times do
+	   Book.create(
+	   	title: Faker::Book.title, 
+	   	description: Faker::Lorem.paragraph, 
+		user_id: rand(1..10),
+		cover_image: Faker::LoremFlickr.image,
+		price: Faker::Commerce.price)
+	end
+end
