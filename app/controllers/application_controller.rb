2ff341d45aae8a2f8fa3162527a2e13905bb05b0
mynameishtoomyataung <htoomyatag.webdev@gmail.com>
2023-02-17 09:53:39 +0700

    render both xml and json added for token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,24 @@
 class ApplicationController < ActionController::API
  include ActionController::MimeResponds
  include JsonWebToken
- 
  before_action :authenticate_request
 
  private
   def authenticate_request
     header = request.headers["Authorization"]
     header = header.split(" ").last if header
-    decoded = jwt_decode(header)
-    @current_user = User.find(decoded[:user_id])
+    begin 
+       decoded = jwt_decode(header)
+       @current_user = User.find(decoded[:user_id])
+      rescue ActiveRecord::RecordNotFound => e
+          respond_to do |format|
+           format.any(:xml, :json) { render request.format.to_sym => {errors: e.message},status: :unauthorized }
+          end
+      rescue JWT::DecodeError => e
+          respond_to do |format|
+           format.any(:xml, :json) { render request.format.to_sym => {errors: e.message},status: :unauthorized }
+          end
+    end
   end
 end
+
--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,14 +1,14 @@
 class AuthenticationController < ApplicationController
 	skip_before_action :authenticate_request
-
 	def login
 		@user = User.find_by(username: params[:username])
-
-		if @user&.authenticate(params[:password])
-			token = jwt_encode(user_id: @user.id)
-			render json: {token: token}, status: :ok
-		else
-			render json: {error: 'unauthorized'}, status: :unauthorized
+		respond_to do |format|
+			if @user&.authenticate(params[:password])
+				token = jwt_encode(user_id: @user.id)
+				format.any(:xml, :json) { render request.format.to_sym => {token: token},status: :ok }
+			else
+				format.any(:xml, :json) { render request.format.to_sym => {error: 'unauthorized'},status: :unauthorized }
+			end
 		end
 	end
 end
